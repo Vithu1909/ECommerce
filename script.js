eecf7c7887aabff76b8626741527db0ad18472be
Vithusan19 <vithu0919@gmail.com>
2024-06-01 16:17:13 +0530

    vithu

--- script.js
+++ script.js
@@ -10,3 +10,38 @@ function closenav()
     sidenav.style.left="-60%";
 
 }
+
+var productcon=document.getElementById("product");
+var search=document.getElementById("search")
+var productlist=productcon.querySelectorAll("div");
+var nofound=document.getElementById("nofound");
+
+search.addEventListener('keyup',function(){
+    var enterdata=event.target.value.toUpperCase();
+    var count =0;
+    for(var i=0;i<productlist.length;i++)
+    {
+        var productname=productlist[i].querySelector('p').textContent;
+        if(productname.toUpperCase().indexOf(enterdata)<0)
+        {
+            productlist[i].style.display="none"
+            count++;
+        }
+        else
+        {
+            productlist[i].style.display="block"
+            count--;
+        }
+    }
+    if(count>=productlist.length)
+    {
+       nofound.style.display="block"
+
+    }
+    else
+    {
+        nofound.style.display="none"
+
+    }
+})  
+
